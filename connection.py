5592890d8d0bbd01af60ca0ef51e51501c9797f6
derlyalfonsor <derly.alfonso-r@mail.escuelaing.edu.co>
2025-04-02 16:59:13 -0500

    Crear nuevas carpetas

---
+++ connection.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
+
+
+def connect_to_mongodb(db_name, collection_name):
+    uri = "mongodb+srv://DerlyMichelleAlfonsoRamirez:Smith2710@derlymichellealfonsoram.9hlwf.mongodb.net/?retryWrites=true&w=majority&appName=DerlyMichelleAlfonsoRamirez"
+    client = MongoClient(uri, server_api=ServerApi('1'))
+    db = client[db_name]
+    collection = db[collection_name]
+    return collection
